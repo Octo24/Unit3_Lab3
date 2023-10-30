dc163fa51d26f38a6d2e5cc12f403dbe73bc0b9d
Octo24 <octavian.hagi-memet@edu.sait.ca>
2023-10-30 14:53:27 -0600

    Calculator complete

---
+++ calculator.py
@@ -0,0 +1,19 @@
+# calculator module
+
+def add(first_number, second_number):
+    result = first_number + second_number
+    print(f"Result: {result}")
+    return result
+
+def subtract(first_number, second_number):
+    result = first_number - second_number
+    print(f"Result: {result}")
+    return result
+
+def multiple(first_number, second_number):
+    result = first_number * second_number
+    print(f"Result: {result}")
+
+def divide(first_number, second_number):
+    result = first_number / second_number
+    print(f"Result: {result}")
